026469b0ce126940ef750f5e47ee8868f1994bd0
Kenneth Powers <ken@kenpowers.net>
2020-02-02 14:37:51 -0500

    refactor: extract testable rewriteUrl function

--- src/handler.ts
+++ src/handler.ts
@@ -1,12 +1,18 @@
-export function handleRequest(request: Request): Response {
-  const { hostname, pathname } = new URL(request.url);
+export function rewriteUrl(url: string): string {
+  const { hostname, pathname } = new URL(url);
   const isNightly = hostname.startsWith('n.') || pathname.startsWith('/n');
 
   const base = `https://doc.rust-lang.org/${
     isNightly ? 'nightly' : 'stable'
   }/std/`;
   const query = pathname.replace(/^(?:\/n)?\//, '');
+
   const finalUrl = `${base}${query ? `?search=${query}` : ''}`;
 
-  return Response.redirect(finalUrl, 301);
+  return finalUrl;
+}
+
+export function handleRequest({ url }: Request): Response {
+  const toUrl = rewriteUrl(url);
+  return Response.redirect(toUrl, 301);
 }

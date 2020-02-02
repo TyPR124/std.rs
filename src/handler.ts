bfc562609c1ffaad9b87cc49e407b77f5a97fff6
Kenneth Powers <ken@kenpowers.net>
2020-02-02 14:57:22 -0500

    fix: fix bugs in detecting nightly vs ^n searches

--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,13 @@
 export function rewriteUrl(url: string): string {
   const { hostname, pathname } = new URL(url);
-  const isNightly = hostname.startsWith('n.') || pathname.startsWith('/n');
+  const isNightly = hostname.startsWith('n.') || /^\/n(?:\/|$)/.test(pathname);
 
   const base = `https://doc.rust-lang.org/${
     isNightly ? 'nightly' : 'stable'
   }/std/`;
-  const query = pathname.replace(/^(?:\/n)?\//, '');
+  // Remove any slashes from the start of the path, as well as /n/ (but not
+  // words starting with /n such as /ne)
+  const query = pathname.replace(/\/n(?:\/|$)/, '').replace(/^\/+/, '');
 
   const finalUrl = `${base}${query ? `?search=${query}` : ''}`;
 

1afaeadad540d535294222f8b535951651043856
Kenneth Powers <ken@kenpowers.net>
2020-02-01 21:35:22 -0500

    feat: implement redirect handler

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,14 @@
-export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`);
+export function handleRequest(request: Request): Response {
+  const { hostname, pathname } = new URL(request.url);
+  const isNightly = hostname.startsWith('n.') || pathname.startsWith('/n');
+
+  const base = `https://doc.rust-lang.org/${
+    isNightly ? 'nightly' : 'stable'
+  }/std/`;
+  const query = pathname.replace(/^(?:\/n)?\//, '');
+  const finalUrl = `${base}${
+    query ? `?search=${encodeURIComponent(query)}` : ''
+  }`;
+
+  return Response.redirect(finalUrl, 301);
 }
